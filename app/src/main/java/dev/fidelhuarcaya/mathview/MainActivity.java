3ecabf1066c807709958b375fc7f28157acd0825
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2023-04-17 22:04:03 -0500

    feat: Add example formula.

--- app/src/main/java/dev/fidelhuarcaya/mathview/MainActivity.java
+++ app/src/main/java/dev/fidelhuarcaya/mathview/MainActivity.java
@@ -10,8 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*MathView mathView= findViewById(R.id.math_view);
-        mathView.setText(sampleText());*/
+        MathView mathView= findViewById(R.id.math_view);
+        mathView.setText(sampleText());
 
     }
 
